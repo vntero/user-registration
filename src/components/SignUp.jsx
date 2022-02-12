1f58cb4cbfd15a43ec2f4820218ddcbf7c6d76ad
vntero <shin.antero@gmail.com>
2022-02-12 22:24:28 +0100

    user data inserted to db successfully

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Input, Card, Spacer, Text, Button } from '@nextui-org/react';
 import {useForm} from 'react-hook-form'
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 function SignUp() {
     
     const {register, handleSubmit} = useForm()
 
     const onSubmit = (data) => {
-        console.log(data)
+        Axios.post("http://localhost:5005/create-user", data)
     }
 
   return (
@@ -17,13 +18,11 @@ function SignUp() {
                 <Card css={{ mw: "400px" }}>
                     <Text h2>Create an account</Text>
                     <br />
-                    <Input clearable underlined placeholder="Enter your name" type='text' name='name' {...register('name')}/>
+                    <Input underlined placeholder="Enter your name" type='text' name='name' {...register('name')}/>
                     <Spacer y={1.5} />
-                    <Input clearable underlined placeholder="What's your email address?" type='email' name='email' {...register('email')}/>
+                    <Input underlined placeholder="What's your email address?" type='email' name='email' {...register('email')}/>
                     <Spacer y={1.5} />
                     <Input.Password underlined labelPlaceholder="Password" name='password' {...register('password')}/>
-                    <Spacer y={1.5} />
-                    <Input.Password underlined labelPlaceholder="Confirm password" name='confirmPassword' {...register('confirmPassword')}/>
                     <Spacer y={1.5} />
                     <Button color="secondary">Sign Up</Button>
                     <Spacer y={1.5} />
